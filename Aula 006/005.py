9a63de555cfec81ae208dcd62a1f3d4326710b53
Hashi1802 <156032305+Hashi1802@users.noreply.github.com>
2024-04-29 20:22:24 -0300

    att python

---
+++ Aula 006/005.py
@@ -0,0 +1,10 @@
+n1 = int(input('Digite um numero: '))
+n2 = int(input('Digite um numero: '))
+s = n1 + n2
+d = n1 - n2
+m = n1 * n2
+p = n1 ** n2
+dv =  n1 / n2
+ds = n1 // n2
+print('a soma é {}, a subtração e {} a multiplicação é {}.'.format(s, d, m)) 
+print('a potenciação é {} ,\n a divisão é {}, a divisão real é{}.'.format(p, dv, ds))
