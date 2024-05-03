bd2b089d89b9ec4cabc94dc6816df9b3603461ce
Hashi1802 <156032305+Hashi1802@users.noreply.github.com>
2024-05-02 21:11:28 -0300

    aula de python

--- Aula 008/ex019.py
+++ Aula 008/ex019.py
@@ -3,6 +3,7 @@ p = input('Primeiro aluno: ')
 s = input('segundo aluno: ')
 t = input('terceiro aluno: ')
 q = input('quarto aluno: ')
+1 = ('anderson')
 lista = [p, s, t, q]
 random.shuffle(lista)
 print('a ordem de apresentação será {}'.format(lista))
