2ab465147f4cbc38ac3e2ef36d4e5cd33e8900ce
Hashi1802 <156032305+Hashi1802@users.noreply.github.com>
2024-06-05 15:10:48 -0300

    aula de python

---
+++ aula 13/ex046.py
@@ -0,0 +1,6 @@
+from time import sleep
+n = int(input(''))
+for c in range(n-1, 0, -1):
+    print(c)
+    sleep(1)
+print('Bum Bum Pow!!!')
--- aula 13/teoria.py
+++ aula 13/teoria.py
@@ -1,1 +1,5 @@
-print('oi')
+s = 0
+for c in range (0,3):
+    n = int(input('Digite um valor: '))
+    s+=n
+print('o somatorio dos valores é {}'.format(s))
