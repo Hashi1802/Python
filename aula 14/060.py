69a2c293265ed01276456bdd98584cb2624b7c6f
Hashi1802 <156032305+Hashi1802@users.noreply.github.com>
2024-06-13 14:07:42 -0300

    aula de python

--- aula 14/060.py
+++ aula 14/060.py
@@ -1,10 +1,11 @@
-n = int(input('''
-Digite um numero para
-calcular o seu fatorial: 
-'''
-))
-p = n - 1
-while p != 1:
-    f = n * p
-    print('{} = {} x {}'.format(n, p, f))
- 
+from math import factorial
+n = int(input('Digite um numero para calcular o seu fatorial:'))
+c = n #contador
+f = 1 #fator nulo de multiplicação
+print('calculando {}! '.format(n), end='')
+while c > 0:
+    print('{}'.format(c), end='')
+    print(' x ' if c > 1 else ' = ', end='') #utilizando if e else em uma unica linha
+    f *= c
+    c -= 1 #contando de forma negativa
+print('{}'.format(f))
---
+++ aula 14/061.py
@@ -0,0 +1,5 @@
+print('GERADOR DE PA')
+print('-==-' * 4)
+p = int(print('Primeiro Termo: '))
+s = int(print('Segundo Termo: '))
+while 
