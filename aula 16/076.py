dea9d74f7c86fcca4339e48175365c4968ff6d1d
Anderson Melo <156032305+Hashi1802@users.noreply.github.com>
2024-07-04 19:48:39 -0300

    aula de python

--- aula 16/076.py
+++ aula 16/076.py
@@ -1,3 +1,10 @@
 listagem = ('Lapis', 1.75, 'Borracha', 2, 'Caderno', 15.90, 'Estojo', 25, 'transferidor', 4.20, 'compasso', 9.99, 'Mochila', 120.32, 'Canetas',22.30, 'livros', 34.90)
 print('-' * 40)
-print(f'{"LISTAGEM DE PREÇOS":ˆ40}')
+print("LISTAGEM DE PREÇOS")
+print('-' * 40)
+for pos in range(0, len(listagem)):
+    if pos % 2 == 0:
+        print(f'{listagem[pos]:.<30}', end='')
+    else:
+        print(f'R${listagem[pos]:>7.2f}')
+print('-' * 40)
