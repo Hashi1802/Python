d51691d81ebec33b04458c2b337433b41389cfd1
Anderson Melo <156032305+Hashi1802@users.noreply.github.com>
2024-07-25 21:09:31 -0300

    aula de python

---
+++ aula 17/ex085.py
@@ -0,0 +1,14 @@
+num = [[], []] ###criei duas listas dentro de uma lista
+valor = 0
+for c in range(7):
+    valor =  int(input('Digite um valor: '))
+    if valor % 2 == 0:
+        num[0].append(valor)
+    else:
+        num[1].append((valor))
+num[0].sort()
+num[1].sort()
+print('-=-' * 15
+)
+print(f'Os valores pares digitados foram {num[0]}')
+print(f'Os valores impares digitados foram {num[1]}')
