03a7852d032e229a58ac334ac99d481dfd68f477
Anderson Melo <156032305+Hashi1802@users.noreply.github.com>
2024-08-12 21:26:46 -0300

    aula 90 de python

--- aula 17/teorico.py
+++ aula 17/teorico.py
@@ -1,2 +1,9 @@
-pessoas = {'nome': 'anderson', 'Sexo':'M', 'idade':29}
-print(f'O {pessoas["nome"]} tem {pessoas["idade"]} anos')
+estado = dict()
+brasil = list()
+for c in range(0, 3):
+    estado['uf'] = str(input('Unidade Federativa: '))
+    estado['Sigla'] = str(input('Sigla do estado: '))
+    brasil.append(estado.copy())
+for e in brasil:
+    for k, v in e.items():
+        print(f'o campo {k} tem valor {v}')
