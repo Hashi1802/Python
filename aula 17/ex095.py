97214fe59711ea10bcdbcadd4c05a5bffd6a1a34
Anderson Melo <156032305+Hashi1802@users.noreply.github.com>
2024-09-18 21:07:24 -0300

    python

--- aula 17/ex095.py
+++ aula 17/ex095.py
@@ -5,8 +5,9 @@ while True:
     jogador.clear()
     jogador['nome'] = str(input('Nome do Jogador: '))
     tot = int(input(f'quantas partidas {jogador["nome"]} jogou? '))
+    partidas.clear()
     for c in range(0, tot):
-        partidas.append(int(input(f'quantos gols na partida {c} ?')))
+        partidas.append(int(input(f'quantos gols na partida {c+1} ?')))
     jogador['gols'] = partidas[:]
     jogador['total'] = sum(partidas)
     time.append(jogador.copy())
@@ -18,13 +19,29 @@ while True:
     if resp == 'N':
         break
 print('-=' * 30)
+print('cod', end='' )
+for i in jogador.keys():
+    print(f'{i:<15}', end='')
+print()
+print('-=' * 30)
 for k, v in enumerate(time):
     print(f'{k:3}', end='')
     for d in v.values():
         print(f'{str(d):<15}', end='')
     print()
-
 print('-=' * 40)
 for k, v in jogador.items():
     print(f' O Campo {k} tem o valor {v}')
 print('-' * 40)
+while True:
+    busca = int(input('Mostrar dados de qual jogador? (999 para parar)'))
+    if busca == 999:
+        break
+    if busca >= len(time):
+        print(f'Erro não existe o jogador {busca}')
+    else:
+        print(f'levantamento do jogador {time[busca]["nome"]}:')
+        for i, g in enumerate(time[busca]['gols']):
+            print(f'   no jogo {i+1} fez {g} gols ')
+    print('-' * 40)
+print('  <<Volte Sempre>>  ')
