37262587b6c57aecf1bc831328baabf250f8aa33
Anderson Melo <156032305+Hashi1802@users.noreply.github.com>
2024-09-25 20:00:43 -0300

    python

--- aula18/ex097.py
+++ aula18/ex097.py
@@ -1,2 +1,9 @@
-def messaget():
-    print('-' * )
+def escreva(txt):
+    variavel = len(txt) + 4
+    print('-' * variavel)
+    print(f'  {txt}')
+    print('-' * variavel)
+    
+
+
+escreva('oi')
---
+++ aula18/ex098.py
@@ -0,0 +1,12 @@
+from time import sleep
+def contador():
+    print('-=' * 30)
+    print('contagem de 1 a te 10 de 1 em 1')
+    for c in range(1,11, 1):
+        print(f'{c} ', end='')
+        sleep(0.5)
+    print('Fim!')
+    print('-=' * 30)
+    for d in range()
+
+contador()
