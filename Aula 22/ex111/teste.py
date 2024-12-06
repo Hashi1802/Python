fd5f18c8f3a4962b6a9e2a41382b7a0c9616c60c
Anderson Melo <156032305+Hashi1802@users.noreply.github.com>
2024-12-06 20:30:43 -0300

    python

---
+++ Aula 22/ex111/teste.py
@@ -0,0 +1,3 @@
+from teste.ex111.utilidade import moeda
+preço = float(input("Digite o preço: "))
+moeda.resumo(preço)
---
+++ teste/ex111/teste.py
@@ -0,0 +1,3 @@
+fr
+preço = float(input("Digite o preço: "))
+moeda.resumo(preço)
